10453e15754cb9106c36b02ca2a0fe68892af171
Jorgearbolex <jorgearbolex@gmail.com>
2019-12-02 18:00:03 -0500

    ahi va
    eso care pepa

---
+++ Assets/scripts/SigueEnemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SigueEnemy : MonoBehaviour
+{
+    public Transform objetivo;
+
+    void FixedUpdate()
+    {
+        transform.LookAt(objetivo);
+
+    }
+    
+}
