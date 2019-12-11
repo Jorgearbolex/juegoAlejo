8c30564a32610a67329e409d4772a3a0d49c8cf2
Jorgearbolex <jorgearbolex@gmail.com>
2019-12-11 16:52:54 -0500

    listo el poio
    listo el poio

---
+++ Assets/scripts/GiraCam.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiraCam : MonoBehaviour
+{
+    public static float rotY;
+   
+
+    void Start()
+    {
+
+    }
+
+    float x = 0;
+    float y = 180;
+
+    void Update()
+    {
+        
+        transform.position = Player.pos;
+        x -= Input.GetAxis("Mouse Y");
+        y += Input.GetAxis("Mouse X");
+        transform.eulerAngles = new Vector3(x, y, 0);
+        rotY = transform.eulerAngles.y;
+    }
+    
+
+}
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+  
+    float speed = 3;
+    public static Vector3 pos;
+   
+
+    void Start()
+    {
+        GameObject empty = new GameObject();
+        empty.name = "Camera";
+        empty.AddComponent<Camera>();
+        empty.AddComponent<GiraCam>();
+        empty.transform.SetParent(transform);
+        empty.transform.localPosition = Vector3.zero;
+        //gameObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
+
+
+    }
+    void Update()
+    {
+        MueveNave();
+    }
+    void MueveNave()
+    {
+        transform.eulerAngles = new Vector3(0, GiraCam.rotY, 0);
+        if (Input.GetKey(KeyCode.W)) { transform.position += transform.forward * (speed / 20); }
+        if (Input.GetKey(KeyCode.S)) { transform.position -= transform.forward * (speed / 20); }
+        if (Input.GetKey(KeyCode.D)) { transform.position += transform.right * (speed / 20); }
+        if (Input.GetKey(KeyCode.A)) { transform.position -= transform.right * (speed / 20); }
+        pos = transform.position;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Finish")
+        {
+            SceneManager.LoadScene("Gonorr");
+        }
+    }
+
+    
+}
